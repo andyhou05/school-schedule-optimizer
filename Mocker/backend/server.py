f457e960cef7f994f78752e1a7a7ef7cf9edb98d
Andy <andy.hou1604@gmail.com>
2024-07-05 22:22:56 -0400

    added CRUD operations for teachers

--- Mocker/backend/server.py
+++ Mocker/backend/server.py
@@ -1,12 +1,60 @@
 from flask import request, jsonify
 from config import app, db
 from models import Course
+from models import Teacher
 
+@app.route("/teachers", methods=["GET"])
+def get_teachers():
+    teachers = Teacher.query.all()
+    json_teachers = list(map(lambda x: x.to_json(), teachers))
+    return jsonify({"teachers":json_teachers}), 200
+
+@app.route("/create_teacher", methods=["POST"])
+def create_teacher():
+    first_name = request.json.get("firstName")
+    last_name = request.json.get("lastName")
+    rating = request.json.get("rating")
+    
+    if not first_name or last_name:
+        return jsonify({"message":"You must enter a first name and a last name"}), 400
+    new_teacher = Teacher(first_name=first_name, last_name=last_name, rating=rating)
+    try:
+        db.session.add(new_teacher)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"message":str(e)}), 400
+    return jsonify({"message":"Teacher created"}), 201
+
+@app.route("/update_teacher/<int:id>", methods=["PATCH"])
+def update_teacher(id):
+    teacher = Teacher.query.get(id)
+    
+    if not teacher:
+        return jsonify({"message":"Teacher not found"}), 404
+    
+    data = request.json
+    teacher.first_name = data.get("firstName", teacher.first_name)
+    teacher.last_name = data.get("lastName", teacher.last_name)
+    teacher.rating = data.get("rating", teacher.rating)
+    db.session.commit()
+    return jsonify({"message":"Teacher updated"}), 200
+
+@app.route("/delete_teacher/<int:id>", methods=["DELETE"])
+def delete_teacher(id):
+    teacher = Teacher.query.get(id)
+    
+    if not teacher:
+        return jsonify({"message":"Teacher not found"}), 404
+    
+    db.session.delete(teacher)
+    db.session.commit()
+    return jsonify({"message":"Teacher deleted"}), 200
+    
 @app.route("/courses", methods=["GET"])
 def get_courses():
     courses = Course.query.all()
     json_courses = list(map(lambda x : x.to_json(), courses))
-    return jsonify({"courses": json_courses})
+    return jsonify({"courses": json_courses}), 200
 
 @app.route("/create_course", methods=["POST"])
 def create_course():
@@ -41,7 +89,7 @@ def update_course(id):
     course.seats = data.get("seats", course.seats)
     
     db.session.commit()
-    return jsonify({"message":"Course updates"}), 200
+    return jsonify({"message":"Course updated"}), 200
 
 @app.route("/delete_course/<int:id>", methods=["DELETE"])
 def delete_course(id):
