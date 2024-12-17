a1d90b650c9d5bb98eea307cf8adfc3e3b437f6e
Andy <andy.hou1604@gmail.com>
2024-12-17 00:55:02 -0500

    refactored using functions

--- frontend/src/components/ScheduleGrid.jsx
+++ frontend/src/components/ScheduleGrid.jsx
@@ -2,17 +2,28 @@ import React from "react";
 import GridLayout from "react-grid-layout";
 
 const ScheduleGrid = () => {
+  const days = [
+    "",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
   const layout = [
-    { i: "1", x: 0, y: 0, w: 1, h: 1, static: true },
-    { i: "2", x: 1, y: 0, w: 1, h: 1, static: true },
-    { i: "3", x: 2, y: 0, w: 1, h: 1, static: true },
-    { i: "4", x: 3, y: 0, w: 1, h: 1, static: true },
-    { i: "5", x: 4, y: 0, w: 1, h: 1, static: true },
-    { i: "6", x: 5, y: 0, w: 1, h: 1, static: true },
-    { i: "7", x: 6, y: 0, w: 1, h: 1, static: true },
-    { i: "8", x: 7, y: 0, w: 1, h: 1, static: true },
+    // Column Headers (days)
+    ...Array.from({ length: 8 }, (_, column) => ({
+      i: `${column + 1}`,
+      x: column,
+      y: 0,
+      w: 1,
+      h: 1,
+      static: true,
+    })),
 
-    ...Array.from({ length: 12 }, (_, row) =>
+    ...Array.from({ length: 21 }, (_, row) =>
       Array.from({ length: 8 }, (_, column) => ({
         i: `cell-${row}-${column}`,
         x: column,
@@ -34,31 +45,14 @@ const ScheduleGrid = () => {
       margin={[1, 0]}
     >
       {/* Column Headers */}
-      <div key="1" style={{ border: "1px solid lightblue" }}></div>
-      <div key="2" style={{ border: "1px solid lightblue" }}>
-        Monday
-      </div>
-      <div key="3" style={{ border: "1px solid lightblue" }}>
-        Tuesday
-      </div>
-      <div key="4" style={{ border: "1px solid lightblue" }}>
-        Wednesday
-      </div>
-      <div key="5" style={{ border: "1px solid lightblue" }}>
-        Thursday
-      </div>
-      <div key="6" style={{ border: "1px solid lightblue" }}>
-        Friday
-      </div>
-      <div key="7" style={{ border: "1px solid lightblue" }}>
-        Saturday
-      </div>
-      <div key="8" style={{ border: "1px solid lightblue" }}>
-        Sunday
-      </div>
+      {Array.from({ length: 8 }, (_, column) => (
+        <div key={`${column + 1}`} style={{ border: "1px solid lightblue" }}>
+          {days[column]}
+        </div>
+      ))}
 
       {/* Dynamic Cells for rows */}
-      {Array.from({ length: 12 }, (_, row) =>
+      {Array.from({ length: 21 }, (_, row) =>
         Array.from({ length: 8 }, (_, column) => (
           <div
             key={`cell-${row}-${column}`}
