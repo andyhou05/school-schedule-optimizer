beee18250ca95ae2ef40e3a3001c0cfd1a96c6fc
Andy <andy.hou1604@gmail.com>
2024-12-17 15:37:39 -0500

    changed RGL to html table

--- frontend/src/components/ScheduleGrid.jsx
+++ frontend/src/components/ScheduleGrid.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import GridLayout from "react-grid-layout";
 
 const ScheduleGrid = () => {
   const columnHeaders = [
@@ -14,66 +13,47 @@ const ScheduleGrid = () => {
   ];
   const minutes = ["00", "30"];
   const times = Array.from({ length: 11 }, (_, hour) =>
-    Array.from({ length: 2 }, (_, index) => `${8 + hour}:${minutes[index]}`)
+    Array.from(minutes, (minute, _) => `${8 + hour}:${minute}`)
   )
     .flat()
     .slice(0, -1);
-  const layout = [
-    // Column Headers (days)
-    ...Array.from({ length: 8 }, (_, column) => ({
-      i: `${column + 1}`,
-      x: column,
-      y: 0,
-      w: 1,
-      h: 1,
-      static: true,
-    })),
-
-    ...Array.from({ length: 21 }, (_, row) =>
-      Array.from({ length: 8 }, (_, column) => ({
-        i: `cell-${row}-${column}`,
-        x: column,
-        y: row + 1,
-        w: 1,
-        h: 1,
-        static: true,
-      }))
-    ).flat(),
-  ];
 
   return (
-    <GridLayout
-      className="layout"
-      layout={layout}
-      cols={8}
-      rowHeight={30}
-      width={1200}
-      margin={[1, 0]}
+    <table
+      style={{
+        border: "1px solid white",
+        width: "50vw",
+        borderCollapse: "collapse",
+      }}
     >
-      {/* Column Headers (days) */}
-      {Array.from({ length: 8 }, (_, column) => (
-        <div key={`${column + 1}`} style={{ border: "1px solid lightblue" }}>
-          {columnHeaders[column]}
-        </div>
-      ))}
-
-      {/* Row Headers (time slots)*/}
-      {Array.from({ length: 21 }, (_, row) => (
-        <div key={`cell-${row}-0`} style={{ border: "1px solid lightblue" }}>
-          {times[row]}
-        </div>
-      ))}
-
-      {/* Cells in grid */}
-      {Array.from({ length: 21 }, (_, row) =>
-        Array.from({ length: 7 }, (_, column) => (
-          <div
-            key={`cell-${row}-${column + 1}`}
-            style={{ border: "1px solid lightblue" }}
-          ></div>
-        ))
-      )}
-    </GridLayout>
+      <thead>
+        <tr>
+          {columnHeaders.map((header, index) => (
+            <th
+              key={index}
+              style={{ border: "1px solid white", padding: "5px" }}
+            >
+              {header}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {Array.from({ length: 21 }, (_, row) => (
+          <tr>
+            <td
+              key={`${row}-0`}
+              style={{ border: "1px solid white", textAlign: "center" }}
+            >
+              {times[row]}
+            </td>
+            {Array.from({ length: 7 }, (_, column) => (
+              <td key={row - column} style={{ border: "1px solid white" }}></td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 export default ScheduleGrid;
