fdf453eb0ca94f41614ce57049ec26c9ad7d6ebd
Andy Hou <andy.hou1604@gmail.com>
2025-04-06 18:41:05 +0200

    nav bar styles

--- frontend/src/components/NavigationBar/NavigationBar.jsx
+++ frontend/src/components/NavigationBar/NavigationBar.jsx
@@ -4,18 +4,19 @@ import { Flex, Box, Tabs } from "@radix-ui/themes";
 import { LandingPage } from "./LandingPage/LandingPage";
 import FAQ from "./FAQ/FAQ";
 import Contact from "./Contact";
+import "../styles/styles.css";
 
 const NavigationBar = () => {
   return (
     <Tabs.Root defaultValue="home">
-      <Box width="fit-content" ml="9" pl="9" mt="5">
+      <Box className="navigation-bar-tabs">
         <Tabs.List>
           <Tabs.Trigger value="home">Home</Tabs.Trigger>
           <Tabs.Trigger value="faq">FAQ</Tabs.Trigger>
           <Tabs.Trigger value="contact">Contact Me</Tabs.Trigger>
         </Tabs.List>
       </Box>
-      <Box height="100vh">
+      <Box className="navigation-bar-page-content">
         <Tabs.Content value="home">
           <LandingPage />
         </Tabs.Content>
