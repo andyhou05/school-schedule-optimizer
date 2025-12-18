9725c35c762a8ac7f660a6d9bbeef7b1e38611f4
Andy Hou <andy.hou1604@gmail.com>
2025-12-17 20:32:10 -0500

    changed db connection string to supabase

--- backend/scripts/db_helper.py
+++ backend/scripts/db_helper.py
@@ -3,8 +3,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import Session
 from dotenv import load_dotenv
+from pathlib import Path
 
 import os
+
+dotenv_path = Path(__file__).parent.parent / ".env"
+load_dotenv(dotenv_path, override=True)
 
 def connect_db() -> Session:
     """
@@ -14,12 +18,14 @@ def connect_db() -> Session:
         Session: Session object that can be used to query database.
     """
     # Environment variables
-    password = os.getenv("MYSQL_PASSWORD")
+    password = os.getenv("DB_PASSWORD")
     host = os.getenv("MOCKER_ENDPOINT")
     user = os.getenv("MOCKER_USER")
     db_name = os.getenv("MOCKER_DB_NAME")
+    port = os.getenv("PORT")
+    DATABASE_URL = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db_name}?sslmode=require"
     
-    engine = create_engine(f"mysql+pymysql://{user}:{password}@{host}/{db_name}")
+    engine = create_engine(DATABASE_URL)
     Session = sessionmaker(bind=engine)
     session = Session()
     return session
@@ -39,3 +45,29 @@ def add_entry(session: Session, entry: Model) -> None:
     except Exception as e:
         print(f"Unable to add to table: {e}\n")
         session.rollback()
+
+
+
+# Testing db connection
+if __name__ == "__main__":
+    password = os.getenv("DB_PASSWORD")
+    host = os.getenv("MOCKER_ENDPOINT")
+    user = os.getenv("MOCKER_USER")
+    db_name = os.getenv("MOCKER_DB_NAME")
+    port = os.getenv("PORT")
+    
+    print("User:", os.getenv("MOCKER_USER"))
+    print("Password:", os.getenv("DB_PASSWORD"))
+    print("Host:", os.getenv("MOCKER_ENDPOINT"))
+    print("Port:", os.getenv("PORT"))
+    print("DB:", os.getenv("MOCKER_DB_NAME"))
+    
+    DATABASE_URL = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db_name}?sslmode=require"
+
+    engine = create_engine(DATABASE_URL)
+
+    try:
+        with engine.connect():
+            print("Connection successful!")
+    except Exception as e:
+        print("Failed to connect:", e)
